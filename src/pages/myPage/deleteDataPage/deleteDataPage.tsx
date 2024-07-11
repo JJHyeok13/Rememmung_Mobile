614334a5db949241e36937ebae92c0e72ba4d4f2
JJHyeok13 <jjinh99@naver.com>
2024-07-11 17:12:54 +0900

    feat: 데이터삭제, 탈퇴 페이지 디자인 수정

--- src/pages/myPage/deleteDataPage/deleteDataPage.tsx
+++ src/pages/myPage/deleteDataPage/deleteDataPage.tsx
@@ -23,32 +23,35 @@ const DeleteDataPage: React.FC = () => {
 
   return (
     <>
-      <div className="flex-1 flex flex-col pt-[46px] px-5">
-        <div className="text-[#E72F2F] font-semibold text-base mb-2">
-          데이터 삭제 전 꼭 읽어주세요!
-        </div>
-        <div className="text-[#444444] font-normal text-sm">
-          데이터를 삭제하시면, 회원은 유지되지만 <br />
-          브리와 나눴던 대화, 사진, 편지 등등 모든 데이터가 사라집니다.
-        </div>
-
-        <div className="flex items-center w-[330px] self-center">
-          <img
-            src={!isChecked ? NotSelectedRadio : SelectedRadio}
-            onClick={handleCheck}
-          />
-          <div className="text-sm font-medium text-black-700">
-            데이터 삭제시, 발생하는 내용을 숙지했어요!
+      <div className="flex-1 flex flex-col pt-[46px] px-5 pb-[42px] h-[100vh] justify-between">
+        <div>
+          <div className="text-[#E72F2F] font-semibold text-base mb-2">
+            데이터 삭제 전 꼭 읽어주세요!
+          </div>
+          <div className="text-[#444444] font-normal text-sm">
+            데이터를 삭제하시면, 회원은 유지되지만 <br />
+            브리와 나눴던 대화, 사진, 편지 등등 모든 데이터가 사라집니다.
           </div>
         </div>
 
-        <div
-          className={`${
-            isChecked ? "bg-brown-500" : "bg-black-300"
-          } text-white font-semibold text-center p-4 w-[330px] rounded-xl cursor-pointer self-center`}
-          onClick={isChecked ? handleModalOpen : undefined}
-        >
-          데이터 삭제하기
+        <div className="flex flex-col">
+          <div className="flex items-center w-[330px] self-center">
+            <img
+              src={!isChecked ? NotSelectedRadio : SelectedRadio}
+              onClick={handleCheck}
+            />
+            <div className="text-sm font-medium text-black-700">
+              데이터 삭제시, 발생하는 내용을 숙지했어요!
+            </div>
+          </div>
+          <div
+            className={`${
+              isChecked ? "bg-brown-500" : "bg-black-300"
+            } text-white font-semibold text-center p-4 w-[330px] rounded-xl cursor-pointer self-center`}
+            onClick={isChecked ? handleModalOpen : undefined}
+          >
+            데이터 삭제하기
+          </div>
         </div>
       </div>
 
