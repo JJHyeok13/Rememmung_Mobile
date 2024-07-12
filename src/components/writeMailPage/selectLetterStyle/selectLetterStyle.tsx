ed878dbfa7406ada3472185b8472841bd7f68d22
JJHyeok13 <jjinh99@naver.com>
2024-07-13 03:19:58 +0900

    feat: 편지쓰기 완료

--- src/components/writeMailPage/selectLetterStyle/selectLetterStyle.tsx
+++ src/components/writeMailPage/selectLetterStyle/selectLetterStyle.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 
-import styles from "./\bstyles";
+import PinkLetterIcon from "@assets/writeMailPage/pinkLetterIcon.svg";
+import GreenLetterIcon from "@assets/writeMailPage/greenLetterIcon.svg";
+import SkyLetterIcon from "@assets/writeMailPage/skyLetterIcon.svg";
+import PurpleLetterIcon from "@assets/writeMailPage/purpleLetterIcon.svg";
 
 const SelectLetterStyle: React.FC = () => {
   return (
-    <styles.Container>
-      <styles.Button />
-      <styles.Button />
-      <styles.Button />
-      <styles.Button />
-      <styles.Button />
-      <styles.Button />
-    </styles.Container>
+    <div className="flex flex-row pl-1 [&_img]:mr-2">
+      <img src={PinkLetterIcon} />
+      <img src={GreenLetterIcon} />
+      <img src={SkyLetterIcon} />
+      <img src={PurpleLetterIcon} />
+    </div>
   );
 };
 
--- src/pages/writeMailPage/writeMailPage.tsx
+++ src/pages/writeMailPage/writeMailPage.tsx
@@ -2,7 +2,6 @@ import React, { ChangeEvent, useEffect, useState } from "react";
 
 import SelectLetterStyle from "@components/writeMailPage/selectLetterStyle/selectLetterStyle";
 
-import styles from "./styles";
 import { writeLetter } from "@server/content/api/letter";
 import { useNavigate } from "react-router-dom";
 import MailNavBar from "@components/writeMailPage/mailNavBar/mailNavBar";
@@ -44,19 +43,17 @@ const WriteMailPage: React.FC = () => {
         <input
           type="text"
           placeholder="제목"
-          className="px-4 py-3 mb-3 bg-black-100 text-black-700 rounded-xl"
+          className="px-4 py-3 mb-3 text-sm bg-black-100 text-black-700 rounded-xl"
           value={letterData.title}
           onChange={handleTitle}
         />
         <textarea
           placeholder="내용"
-          className="px-4 py-3 resize-none bg-black-100 text-black-700 rounded-xl min-h-[313px] mb-4"
+          className="px-4 py-3 resize-none bg-black-100 text-sm text-black-700 rounded-xl h-[313px] mb-4"
           value={letterData.content}
           onChange={handleContent}
         />
-        <styles.LowerContainer>
-          <SelectLetterStyle />
-        </styles.LowerContainer>
+        <SelectLetterStyle />
       </div>
       <div
         className="px-8 py-3 text-center text-white bg-brown-500 rounded-xl"
